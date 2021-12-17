353060fbc39b6491429934905031a16f59557167
Guilherme Oliveira <guilherme.oliveira@eroad.com>
2021-12-17 15:25:34 +1300

    Ignore typescript nodes

--- src/babel-plugin-rewire.js
+++ src/babel-plugin-rewire.js
@@ -31,7 +31,14 @@ module.exports = function({ types: t, template }) {
 		!(parent.type === 'ExportSpecifier') &&
 		!(parent.type === 'ImportSpecifier') &&
 		!(parent.type === 'ObjectTypeProperty') &&
-		!(parent.type === 'ClassMethod')
+		!(parent.type === 'ClassMethod') &&
+		!(parent.type === 'TSDeclareFunction') &&
+		!(parent.type === 'TSExpressionWithTypeArguments') &&
+		!(parent.type === 'TSQualifiedName') &&
+		!(parent.type === 'TSTypeQuery') &&
+		!(parent.type === 'TSTypeReference') &&
+		!(parent.type === 'TSInterfaceDeclaration') &&
+		!(parent.type === 'TSTypeAliasDeclaration');
 	}
 
 	function doesIdentifierRepresentAValidReference(path, variableBinding, rewireInformation) {
